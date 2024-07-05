3ecf259044fa873f48587887eacfa42094fe43c5
AmiasYaska <amiasyaska@gmail.com>
2024-07-05 18:44:02 +0300

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_05_143030) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_05_153916) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_05_143030) do
     t.bigint "customer_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["customer_id"], name: "index_rides_on_customer_id"
   end
 
