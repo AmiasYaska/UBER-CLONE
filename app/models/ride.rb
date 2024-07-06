62c774dbdccd6f8230d1c5b80bb6826031f53cc5
AmiasYaska <amiasyaska@gmail.com>
2024-07-06 14:10:26 +0300

    changes

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,5 +1,5 @@
 class Ride < ApplicationRecord
   belongs_to :customer
   belongs_to :driver, optional: true
-  enum status: [:pending, :in_progress, :cancelled]
+  enum status: [:pending, :in_progress, :completed, :cancelled]
 end
